a43f2ed0a9c8d16b2609976151c19279be273847
Chelle <chellecol@gmail.com>
2021-06-01 08:49:37 -0400

    Refactor style

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,10 +1,11 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import './ProjectDetail.css';
 
 const ProjectDetail = ({ project }) => {
     return (
-        <Card variant='top' style={{width: '30rem'}} className='m-2'>
-            <Card.Img src={project.image} style={{maxHeight: '15rem', objectFit: 'cover'}} />
+        <Card variant='top' className='ProjectDetail-Card m-2'>
+            <Card.Img src={project.image} className='ProjectDetail-Image'/>
             <Card.Title className='m-2'>{project.title}</Card.Title>
             <Card.Text className='m-2'>{`${project.desc}`}</Card.Text>
             <Button href={`${project.url}`}>Visit</Button>
